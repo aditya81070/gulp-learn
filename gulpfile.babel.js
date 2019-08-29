7f1c0ae8cbc42fed336ac794a5e4bb6b87a9358f
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-08-29 22:32:49 +0530

    setup gulp for css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,21 @@ import gulp from 'gulp'
 import browserSync from 'browser-sync'
 import htmlmin from 'gulp-htmlmin'
 import del from 'del'
+import postcss from 'gulp-postcss'
+import autoprefixer from 'autoprefixer'
+import sorting from 'postcss-sorting'
+import cleanCSS from 'gulp-clean-css'
+
 const server = browserSync.create()
 
 const paths = {
   html: {
     src: 'src/*.html',
     dest: 'dist/'
+  },
+  styles: {
+    src: 'src/assets/styles/**/*.css',
+    dest: 'dist/assets/styles/'
   }
 }
 
@@ -36,9 +45,26 @@ const copyHtml = () => {
     .pipe(gulp.dest(paths.html.dest))
 }
 
-const watchFiles = () => gulp.watch(paths.html.src, gulp.series(copyHtml, reload))
+const styles = () => {
+  return gulp.src(paths.styles.src)
+    .pipe(postcss([
+      autoprefixer(),
+      sorting({
+        'properties-order': 'alphabetical'
+      })
+    ]))
+    .pipe(cleanCSS({
+      format: 'beautify'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+}
 
-const dev = gulp.series(clean, copyHtml, serve, watchFiles)
+const watchFiles = () => {
+  gulp.watch(paths.html.src, gulp.series(copyHtml, reload))
+  gulp.watch(paths.styles.src, gulp.series(styles, reload))
+}
+
+const dev = gulp.series(clean, copyHtml, styles, serve, watchFiles)
 
 export default dev
-export { copyHtml, serve, reload, watchFiles }
+export { copyHtml, serve, reload, watchFiles, styles }
